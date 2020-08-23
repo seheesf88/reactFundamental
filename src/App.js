823b48186905a3bb6e368f4cb97486eb43411b41
SeHee Son <seheeson@MacBook-Pro.local>
2020-08-22 23:19:01 -0700

    about react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+
+greeting = () => {
+  alert('hi')
+}
+
+
+
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Fundamental</h1>
+      <button onClick={this.greeting}>hi</button>
     </div>
+
   );
+}
 }
 
 export default App;
